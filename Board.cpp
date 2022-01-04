3cdb31f72bb2f57ee19edf99f97f08d41507a7d2
MatteoTonello <tmatteo1127@gmail.com>
2022-01-04 22:33:09 +0100

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -1,3 +1,24 @@
+Board::Board()
+{
+    gameboard=new Piece*[8][8];
+    gameboard[0][0]=new Rock();
+    gameboard[][]=new Knight();
+    gameboard[][]=new Bishop();
+    gameboard[][]=new Queen();
+    gameboard[][]=new King();
+    gameboard[][]=new Bishop();
+    gameboard[][]=new Knight();
+    gameboard[][]=new Rock();
+    gameboard[0][0]=new Rock();
+    gameboard[][]=new Knight();
+    gameboard[][]=new Bishop();
+    gameboard[][]=new Queen();
+    gameboard[][]=new King();
+    gameboard[][]=new Bishop();
+    gameboard[][]=new Knight();
+    gameboard[][]=new Rock();
+
+}
 int Board::check_draw_for_ripetions();
 bool Board::is_check_mate(char c)
 {

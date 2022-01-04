4fc8621ef45a351df86f1248f55b64054a2ed8e9
MatteoTonello <tmatteo1127@gmail.com>
2022-01-04 23:52:39 +0100

    Update Piece.h

--- Piece.h
+++ Piece.h
@@ -1,11 +1,15 @@
+#ifndef PIECE_H
+#define PIECE_H
+#include "Board.cpp"
 class Piece
 {
 	public:
-		const char piece;
+		char piece;
 		int number;
 		int letter;
 		Board b;
-		const char color;
+		char color;
 		virtual void move(int n, int l) =0;
 		virtual bool can_move() =0;
 };
+#endif

7a7fee4fb705461b94884c802cc61dbafd39f87c
MatteoTonello <tmatteo1127@gmail.com>
2022-01-04 23:52:36 +0100

    Update Board.h

--- Board.h
+++ Board.h
@@ -1,14 +1,19 @@
+#ifndef BOARD_H
+#define BOARD_H
+#include "Piece.h"
+#include <vector>
+using namespace std;
 class Board
 {
-	private:
-		Piece* gameboard[8][8];
-		vector<b> last_bs;
 	public:
+		Piece* gameboard[8][8];
+		vector<Board> last_bs;
 		int check_draw_for_ripetions();
-		bool is_check_mate();
+		bool is_check_mate(char c);
 		bool is_check(char c);
 		bool is_draw();
 		int white_pieces;
 		int black_pieces;
 		
-}
+};
+#endif

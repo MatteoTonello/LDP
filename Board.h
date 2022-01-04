0ee804c3f54409f3af148fb5640bc8bb49d0dafd
MatteoTonello <tmatteo1127@gmail.com>
2022-01-04 19:02:43 +0100

    Create Board.h
    Game .h

---
+++ Board.h
@@ -0,0 +1,14 @@
+class Board
+{
+	private:
+		Piece* gameboard[8][8];
+		vector<b> last_bs;
+	public:
+		int check_draw_for_ripetions;
+		bool is_check_mate();
+		bool is_check();
+		bool is_draw();
+		int white_pieces;
+		int black_pieces;
+		
+}

9b2af84e931c13eae1d4dc785c5a20e58ff1d8d2
MatteoTonello <tmatteo1127@gmail.com>
2022-01-04 19:40:56 +0100

    Update Board.h

--- Board.h
+++ Board.h
@@ -6,7 +6,7 @@ class Board
 	public:
 		int check_draw_for_ripetions;
 		bool is_check_mate();
-		bool is_check();
+		bool is_check(char c);
 		bool is_draw();
 		int white_pieces;
 		int black_pieces;

fc3efaaf231dd1da610159efd2944f1bf81ce1cb
MatteoTonello <tmatteo1127@gmail.com>
2022-01-04 23:52:42 +0100

    Update Pawn.h

--- Pawn.h
+++ Pawn.h
@@ -1,6 +1,15 @@
+
+#ifndef PAWN_H
+#define PAWN_H
 #include <iostream>
+#include "Piece.h"
 using namespace std;
-class Pawn::public Piece
+class Pawn : public Piece
 {
+    public:
     Pawn(int l, int n, char col, Board& myBoard);
+    void move(int n, int l);
+    bool can_move();
+
 };
+#endif

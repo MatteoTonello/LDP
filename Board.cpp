04a7f606943a9a8dbc1d735c28f0fc5dcf9a95a3
MatteoTonello <tmatteo1127@gmail.com>
2022-01-04 19:53:28 +0100

    Create Board.cpp

---
+++ Board.cpp
@@ -0,0 +1,12 @@
+int Board::check_draw_for_ripetions();
+bool Board::is_check_mate(char c)
+{
+    if(is_check(c))
+    {
+        
+    }
+}
+bool Board::is_check(char c);
+bool Board::is_draw();
+int Board::white_pieces;
+int Board::black_pieces;

aee2cdf2d6b31e59921b4e8102ead2d9b4a078ed
DedeBen <daniele.francescato.1@studenti.unipd.it>
2022-01-05 01:42:07 +0100

    Update Board.h

--- Board.h
+++ Board.h
@@ -1,19 +1,16 @@
-#ifndef BOARD_H
-#define BOARD_H
-#include <vector>
-using namespace std;
-class Piece;
 class Board
 {
-	public:
+	private:
 		Piece* gameboard[8][8];
-		vector<Board> last_bs;
+		vector<b> last_bs;
+		vector<Piece*> whites;
+		vector<Piece*> blacks;
+	public:
 		int check_draw_for_ripetions();
-		bool is_check_mate(char c);
+		bool is_check_mate();
 		bool is_check(char c);
 		bool is_draw();
 		int white_pieces;
 		int black_pieces;
 		
-};
-#endif
+}

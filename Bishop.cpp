33e1b74d5d0570f053b3a28bb8962560b3092585
DedeBen <daniele.francescato.1@studenti.unipd.it>
2022-01-05 12:13:03 +0100

    Create Bishop.cpp

---
+++ Bishop.cpp
@@ -0,0 +1,81 @@
+#ifndef BISHOP_CPP
+#define BISHOP_CPP
+#include "Illegal_move.cpp"
+#include "Bishop.h"
+#include <cstdlib>
+using namespace std;
+   Bishop::Bishop(int l, int n, char col, Board& myBoard)
+   {
+      number=n;
+      letter=l;
+      color=col;
+      b=myBoard;
+      if(color=='w') piece='a';
+      else piece='A';
+   };
+
+   bool Bishop::can_move()
+   {
+      b.gameboard[letter][number]=nullptr;
+      if(b.is_check()) return false;
+      b.gameboard[letter][number]=this;
+      for(int i=-1; i<=1; i+2)
+      {
+         for(int j=-1;j<=1;j+2)
+         {
+            if(b.gameboard[letter+i][number+j]==nullptr) return true;
+            if(b.gameboard[letter+i][number+j]->color!=color) return true;
+         }
+      }
+      return false;
+   }
+
+   void Bishop::move(int l, int n)
+   {
+      if(b.is_check()) throw invalid_move();
+      if((abs(l-letter))!=(abs(n-number))) throw invalid_move();
+      
+      if(n>number)
+      {
+         if(l>letter)
+         {
+            for(int i=number, j=letter; i<=n; i++, j++)
+            {
+               if(b.gameboard[i][j]!=nullptr) throw invalid_move();
+            }
+         }
+         else
+         {
+            for(int i=number, j=letter; i<=n; i++, j--)
+            {
+               if(b.gameboard[i][j]!=nullptr) throw invalid_move();
+            }
+         }
+      }
+      else
+      {
+         if(l>letter)
+         {
+            for(int i=number, j=letter; i<=n; i--, j++)
+            {
+               if(b.gameboard[i][j]!=nullptr) throw invalid_move();
+            }
+         }
+         else
+         {
+            for(int i=number, j=letter; i<=n; i--, j--)
+            {
+               if(b.gameboard[i][j]!=nullptr) throw invalid_move();
+            }
+         }
+      }
+      if(b.gameboard[l][n]!=nullptr)
+      {
+         if(b.gameboard[l][n]->color==color)throw invalid_move();
+      }
+      b.gameboard[letter][number]=nullptr;
+      letter=l; number=n;
+      b.gameboard[l][n]=this;
+      }
+
+}

5fadd6c0132ab5a9bfe5de949f5e962c975eb32c
DedeBen <daniele.francescato.1@studenti.unipd.it>
2022-01-05 11:11:20 +0100

    Update Knight.cpp

--- Knight.cpp
+++ Knight.cpp
@@ -2,6 +2,7 @@
 #define KNIGHT_CPP
 #include "Illegal_move.cpp"
 #include "Knight.h"
+#include <cstdlib>
 using namespace std;
 Knight::Knight(int l, int n, char col, Board& myBoard)
 {
@@ -14,14 +15,21 @@ Knight::Knight(int l, int n, char col, Board& myBoard)
 };
 
 bool Knight::can_move()
-		{
-			b.gameboard[letter][number]=nullptr;
-			if(b.is_check(color))return false;
-			b.gameboard[letter][number+1]=this;
-		};
+	{
+		b.gameboard[letter][number]=nullptr;
+		if(b.is_check(color))return false;	//controllo scacco
+		b.gameboard[letter][number+1]=this;
+	};
 void Knight::move(int n, int l)
-		{
-         if(l-letter>3 || l-letter<-3 || n-number>3 || n-number<-3) throw new Illegal_move();
-         
+	{
+		if((abs(l-letter)==3 && abs(n-number)==1) || (abs(l-letter)==1 && abs(n-number)==3)){ //controllo se non è una delle 8 caselle possibili
+			if(b.gameboard[l][n]==nullptr) if(b.gameboard[l][n]->color==color) throw new Illegal_move();	//controllo se è lo stesso colore
+			b.gameboard[letter][number]=nullptr;	//Se non è uguale effettuo la modifica
+			number=n;
+			letter=l;
+			b.gameboard[l][n]=this;
+			return;
 		}
+		else throw new Illegal_move();
+	}
 #endif
